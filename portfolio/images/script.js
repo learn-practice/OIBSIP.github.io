754275a115d717aedfc1a0ded1fb2601291e1835
Deshmukh Vaishnavi <118742532+learn-practice@users.noreply.github.com>
2023-11-15 13:24:55 +0530

    Add files via upload

---
+++ portfolio/images/script.js
@@ -0,0 +1,57 @@
+function show()
+{
+    var htmlbox = document.getElementById("htmlbox");
+    
+    htmlbox.style.display = "flex";
+    htmlbox.style.transition = "1s all linear";
+    htmlbox.style.transform = "sacle(0.5,0.5)";
+    htmlbox.style.animation = "display 2s linear";    
+}
+function hide()
+{
+    var htmlbox = document.getElementById("htmlbox");
+    htmlbox.style.display = "none";
+}
+function show1()
+{
+    var htmlbox = document.getElementById("cssbox");
+    
+    htmlbox.style.display = "flex";
+    htmlbox.style.transition = "1s all linear";
+    htmlbox.style.transform = "sacle(0.5,0.5)";
+    htmlbox.style.animation = "display1 2s linear";    
+}
+function hide1()
+{
+    var htmlbox = document.getElementById("cssbox");
+    htmlbox.style.display = "none";
+}
+
+function show2()
+{
+    var htmlbox = document.getElementById("jsbox");
+    
+    htmlbox.style.display = "flex";
+    htmlbox.style.transition = "1s all linear";
+    htmlbox.style.transform = "sacle(0.5,0.5)";
+    htmlbox.style.animation = "display2 2s linear";    
+}
+function hide2()
+{
+    var htmlbox = document.getElementById("jsbox");
+    htmlbox.style.display = "none";
+}
+function show4()
+{
+    var htmlbox = document.getElementById("javabox");
+    
+    htmlbox.style.display = "flex";
+    htmlbox.style.transition = "1s all linear";
+    htmlbox.style.transform = "sacle(0.5,0.5)";
+    htmlbox.style.animation = "display2 2s linear";    
+}
+function hide4()
+{
+    var htmlbox = document.getElementById("javabox");
+    htmlbox.style.display = "none";
+}
